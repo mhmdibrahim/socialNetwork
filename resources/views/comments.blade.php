370bb835c97d2cf43bd1ceff755dc6c5a7853c52
unknown <devmhmd0@gmail.com>
2018-10-07 19:25:30 +0200

    Working on comments likes

--- resources/views/comments.blade.php
+++ resources/views/comments.blade.php
@@ -12,9 +12,14 @@
             <tbody>
             @forelse($comments as $comment)
                 <tr>
-                    <td>{{$comment->body}}</td>
+                    <td>{{$comment->body}}
+                    <a href="#" class="btn btn-info">Like</a>
+                        <br><br>
+                    <a href="#" class="btn-primary">Show All Likes</a>
+                    </td>
                     <td>@if($comment->user->name === auth()->user()->name)  <b>Me</b> @else <b> {{$comment->user->name}} </b> @endif
-                    @if($post->user_id == auth()->user()->id || (auth()->user()->id == $comment->user->id))<a class="btn btn-danger float-md-right" href="/comment/{{$comment->id}}/delete">Delete</a>@endif
+                    @if($post->user_id == auth()->user()->id || (auth()->user()->id == $comment->user->id))
+                            <a class="btn btn-danger float-md-right" href="/comment/{{$comment->id}}/delete">Delete</a>@endif
                     </td>
                 </tr>
                 @empty
